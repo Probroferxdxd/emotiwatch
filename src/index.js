6d8a6a1c3bdaaf5e2ebe9c49a3d432bddf0c2e68
Probroferxdxd . <probrofer@gmail.com>
2025-07-16 16:03:22 -0500

    decimo commit

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRoute basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+  </BrowserRoute>
 );
 
 // If you want to start measuring performance in your app, pass a function
